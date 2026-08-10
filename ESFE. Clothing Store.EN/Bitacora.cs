cce11d468695c3332d405c23b8b3f7d2814e218b
omare <omare@LENOVO>
2026-08-10 11:36:08 -0600

    Creacion de clase clientes y bitacora

---
+++ ESFE. Clothing Store.EN/Bitacora.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE._Clothing_Store.EN
+{
+    public class Bitacora
+    {
+        public int id_actividad { get; set; }
+        public string Accion { get; set; }
+        public int Id_Usuario { get; set; }
+        public DateTime Fecha_y_hora { get; set; }
+    }
+}
---
+++ ESFE. Clothing Store.EN/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE._Clothing_Store.EN
+{
+    public class Clientes
+    {
+        public int id_cliente { get; set; }
+        public string Nombre { get; set; }
+        public int Dui { get; set; }
+        public int Telefono { get; set; }
+        public string Correo { get; set; }
+        public int id_rol { get; set; }
+        public int id_permiso { get; set; }
+        public int id_estado { get; set; }
+    }
+}
