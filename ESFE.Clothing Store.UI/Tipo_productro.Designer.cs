c979c94210aed12bb89f347cab54fc82531d8d3f
Alumno <Alumno@M12-CIII>
2026-08-13 11:05:07 -0600

    CREACION DE TABLA TIPO PRODUCTO

---
+++ ESFE.Clothing Store.UI/Tipo_productro.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ESFE.Clothing_Store.UI
+{
+    partial class Tipo_productro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.ImageAlign = ContentAlignment.TopLeft;
+            label1.Location = new Point(1, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Tipo Producto";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveCaption;
+            label2.Location = new Point(1, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(216, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Busqueda por nombre o id de producto";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ActiveCaption;
+            label3.Location = new Point(414, 48);
+            label3.Name = "label3";
+            label3.Size = new Size(127, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Busqueda de producto";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ActiveCaption;
+            label4.Location = new Point(1, 87);
+            label4.Name = "label4";
+            label4.Size = new Size(105, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Limpiar busqueda ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ActiveCaption;
+            label5.Location = new Point(1, 138);
+            label5.Name = "label5";
+            label5.Size = new Size(71, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Id_Producto";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.ActiveCaption;
+            label6.Location = new Point(1, 196);
+            label6.Name = "label6";
+            label6.Size = new Size(56, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Producto";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = SystemColors.ActiveCaption;
+            label7.Location = new Point(1, 259);
+            label7.Name = "label7";
+            label7.Size = new Size(278, 15);
+            label7.TabIndex = 6;
+            label7.Text = "Eliminar producto por nombre o id (Administrador)";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = SystemColors.ActiveCaption;
+            label8.Location = new Point(432, 259);
+            label8.Name = "label8";
+            label8.Size = new Size(102, 15);
+            label8.TabIndex = 7;
+            label8.Text = "Eliminar producto";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(223, 40);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(185, 23);
+            textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(138, 79);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(191, 23);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(98, 130);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(181, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(285, 251);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(141, 23);
+            textBox4.TabIndex = 11;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // Tipo_productro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 300);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Tipo_productro";
+            Text = "Tipo_productro";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+    }
+}
---
+++ ESFE.Clothing Store.UI/Tipo_productro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ESFE.Clothing_Store.UI
+{
+    public partial class Tipo_productro : Form
+    {
+        public Tipo_productro()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
