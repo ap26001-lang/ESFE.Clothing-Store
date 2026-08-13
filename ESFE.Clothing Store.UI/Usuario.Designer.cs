6a749041d8a2e50d7299dcd81ac5ca73725b204b
Alumno <Alumno@M12-CIII>
2026-08-13 11:19:24 -0600

    CREACION DE LA TABLA DE USUARIO

---
+++ ESFE.Clothing Store.UI/Usuario.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace ESFE.Clothing_Store.UI
+{
+    partial class Usuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(2, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveCaptionText;
+            label2.ForeColor = SystemColors.ButtonFace;
+            label2.Location = new Point(2, 51);
+            label2.Name = "label2";
+            label2.Size = new Size(250, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Buscar por nombre de usuario o id de usuario ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ActiveCaptionText;
+            label3.ForeColor = SystemColors.ButtonFace;
+            label3.Location = new Point(419, 51);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Buscar usuario";
+            // 
+            // textBox1
+            // 
+            textBox1.ForeColor = SystemColors.ButtonFace;
+            textBox1.Location = new Point(249, 43);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(172, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ActiveCaptionText;
+            label4.ForeColor = SystemColors.ButtonFace;
+            label4.Location = new Point(2, 101);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Limpiar";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ActiveCaptionText;
+            label5.ForeColor = SystemColors.ButtonFace;
+            label5.Location = new Point(2, 144);
+            label5.Name = "label5";
+            label5.Size = new Size(61, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Id_usuario";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.ActiveCaptionText;
+            label6.ForeColor = SystemColors.ButtonFace;
+            label6.Location = new Point(2, 183);
+            label6.Name = "label6";
+            label6.Size = new Size(47, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Usuario";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = SystemColors.ActiveCaptionText;
+            label7.ForeColor = SystemColors.ButtonFace;
+            label7.Location = new Point(2, 227);
+            label7.Name = "label7";
+            label7.Size = new Size(194, 15);
+            label7.TabIndex = 7;
+            label7.Text = "Eliminar por usuario o id de usuario";
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = SystemColors.ActiveCaptionText;
+            label8.ForeColor = SystemColors.ButtonFace;
+            label8.Location = new Point(329, 227);
+            label8.Name = "label8";
+            label8.Size = new Size(92, 15);
+            label8.TabIndex = 8;
+            label8.Text = "Eliminar usuario";
+            // 
+            // textBox2
+            // 
+            textBox2.ForeColor = SystemColors.ButtonFace;
+            textBox2.Location = new Point(192, 219);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(140, 23);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.ForeColor = SystemColors.ButtonFace;
+            textBox3.Location = new Point(72, 136);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(124, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.ForeColor = SystemColors.ButtonFace;
+            textBox4.Location = new Point(55, 180);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(156, 23);
+            textBox4.TabIndex = 11;
+            // 
+            // Usuario
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 292);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Usuario";
+            Text = "Usuario";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+    }
+}
---
+++ ESFE.Clothing Store.UI/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ESFE.Clothing_Store.UI
+{
+    public partial class Usuario : Form
+    {
+        public Usuario()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
