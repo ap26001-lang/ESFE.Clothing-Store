178cb0ec4943088cd6ee6cae0158b87d643b3ff5
Alumno <Alumno@M12-CIII>
2026-08-17 10:46:51 -0600

    CREACION DE FORMULARIO DE VENTAS

---
+++ ESFE.Clothing Store.UI/Ventas.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ESFE.Clothing_Store.UI
+{
+    partial class Ventas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.GradientActiveCaption;
+            label1.Location = new Point(2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Ventas";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.GradientActiveCaption;
+            label2.Location = new Point(2, 33);
+            label2.Name = "label2";
+            label2.Size = new Size(267, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Busqueda por id de venta o nombre del producto";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.GradientActiveCaption;
+            label3.Location = new Point(2, 78);
+            label3.Name = "label3";
+            label3.Size = new Size(132, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Fecha y Hora de Ventas ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.GradientActiveCaption;
+            label4.Location = new Point(3, 115);
+            label4.Name = "label4";
+            label4.Size = new Size(131, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Cantidad de productos ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.GradientActiveCaption;
+            label5.Location = new Point(2, 151);
+            label5.Name = "label5";
+            label5.Size = new Size(108, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Tipo de Producto   ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.GradientActiveCaption;
+            label6.Location = new Point(3, 183);
+            label6.Name = "label6";
+            label6.Size = new Size(44, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Cliente";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(258, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(183, 23);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(244, 107);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(197, 23);
+            textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(329, 25);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(181, 23);
+            textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(229, 143);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(212, 23);
+            textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(229, 180);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(192, 23);
+            textBox5.TabIndex = 14;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(35, 248);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 15;
+            button1.Text = "Guardar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Location = new Point(604, 33);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 16;
+            button2.Text = "Buscar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.Location = new Point(143, 248);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 17;
+            button3.Text = "Eliminar";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // Ventas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 283);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Ventas";
+            Load += Ventas_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ ESFE.Clothing Store.UI/Ventas.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Drawing;
+using Color = System.Drawing.Color;
+using System.Windows.Forms;
+using ESFE._Clothing_Store.EN;
+
+namespace ESFE.Clothing_Store.UI
+{
+    public partial class Ventas : Form
+    {
+        // Controles
+        private Label lblTitulo;
+        private Label lblCodigo;
+        private Label lblFecha;
+        private Label lblProducto;
+        private Label lblCantidad;
+        private Label lblCliente;
+
+        private TextBox txtCodigo;
+        private DateTimePicker dtpFecha;
+        private ComboBox cmbProducto;
+        private NumericUpDown nudCantidad;
+        private ComboBox cmbCliente;
+
+        private Button btnGuardar;
+        private Button btnLimpiar;
+
+        public Ventas()
+        {
+            InitializeComponent();
+            DiseñarFormulario();
+        }
+
+        private void Ventas_Load(object sender, EventArgs e)
+        {
+            // Handler asociado desde el diseñador; sin lógica adicional por ahora.
+        }
+
+
+
+        private void DiseñarFormulario()
+        {
+            // ==============================
+            // CONFIGURACIÓN DEL FORMULARIO
+            // ==============================
+
+            this.Text = "ESFE Clothing Store - Registro de Ventas";
+            this.Size = new Size(760, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.BackColor = Color.FromArgb(245, 246, 248);
+
+            // ==============================
+            // ENCABEZADO
+            // ==============================
+
+            Panel panelTitulo = new Panel();
+            panelTitulo.Location = new Point(0, 0);
+            panelTitulo.Size = new Size(760, 90);
+            panelTitulo.BackColor = Color.FromArgb(35, 39, 47);
+
+            lblTitulo = new Label();
+            lblTitulo.Text = "REGISTRO DE VENTA";
+            lblTitulo.Font = new Font("Segoe UI", 22, FontStyle.Bold);
+            lblTitulo.ForeColor = Color.White;
+            lblTitulo.Location = new Point(35, 18);
+            lblTitulo.AutoSize = true;
+
+            Label lblSubtitulo = new Label();
+            lblSubtitulo.Text = "ESFE Clothing Store";
+            lblSubtitulo.Font = new Font("Segoe UI", 10);
+            lblSubtitulo.ForeColor = Color.LightGray;
+            lblSubtitulo.Location = new Point(38, 57);
+            lblSubtitulo.AutoSize = true;
+
+            panelTitulo.Controls.Add(lblTitulo);
+            panelTitulo.Controls.Add(lblSubtitulo);
+
+            this.Controls.Add(panelTitulo);
+
+            // ==============================
+            // CÓDIGO DE VENTA
+            // ==============================
+
+            lblCodigo = CrearLabel("Código de venta", 60, 125);
+
+            txtCodigo = new TextBox();
+            txtCodigo.Location = new Point(60, 150);
+            txtCodigo.Size = new Size(280, 30);
+            txtCodigo.Font = new Font("Segoe UI", 11);
+            txtCodigo.PlaceholderText = "Ejemplo: VTA-0001";
+
+            this.Controls.Add(lblCodigo);
+            this.Controls.Add(txtCodigo);
+
+            // ==============================
+            // FECHA Y HORA
+            // ==============================
+
+            lblFecha = CrearLabel("Fecha y hora", 390, 125);
+
+            dtpFecha = new DateTimePicker();
+            dtpFecha.Location = new Point(390, 150);
+            dtpFecha.Size = new Size(280, 30);
+            dtpFecha.Font = new Font("Segoe UI", 10);
+            dtpFecha.Format = DateTimePickerFormat.Custom;
+            dtpFecha.CustomFormat = "dd/MM/yyyy HH:mm";
+            dtpFecha.Value = DateTime.Now;
+
+            this.Controls.Add(lblFecha);
+            this.Controls.Add(dtpFecha);
+
+            // ==============================
+            // TIPO DE PRODUCTO
+            // ==============================
+
+            lblProducto = CrearLabel("Tipo de producto", 60, 210);
+
+            cmbProducto = new ComboBox();
+            cmbProducto.Location = new Point(60, 235);
+            cmbProducto.Size = new Size(280, 30);
+            cmbProducto.Font = new Font("Segoe UI", 10);
+            cmbProducto.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Datos de ejemplo
+            cmbProducto.Items.Add("Camisa");
+            cmbProducto.Items.Add("Pantalón");
+            cmbProducto.Items.Add("Vestido");
+            cmbProducto.Items.Add("Zapatos");
+            cmbProducto.Items.Add("Accesorios");
+
+            this.Controls.Add(lblProducto);
+            this.Controls.Add(cmbProducto);
+
+            // ==============================
+            // CANTIDAD
+            // ==============================
+
+            lblCantidad = CrearLabel("Cantidad de productos", 390, 210);
+
+            nudCantidad = new NumericUpDown();
+            nudCantidad.Location = new Point(390, 235);
+            nudCantidad.Size = new Size(280, 30);
+            nudCantidad.Font = new Font("Segoe UI", 10);
+            nudCantidad.Minimum = 1;
+            nudCantidad.Maximum = 999;
+            nudCantidad.Value = 1;
+
+            this.Controls.Add(lblCantidad);
+            this.Controls.Add(nudCantidad);
+
+            // ==============================
+            // CLIENTE
+            // ==============================
+
+            lblCliente = CrearLabel("Cliente", 60, 295);
+
+            cmbCliente = new ComboBox();
+            cmbCliente.Location = new Point(60, 320);
+            cmbCliente.Size = new Size(610, 30);
+            cmbCliente.Font = new Font("Segoe UI", 10);
+            cmbCliente.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Datos de ejemplo
+            cmbCliente.Items.Add("Cliente 1");
+            cmbCliente.Items.Add("Cliente 2");
+            cmbCliente.Items.Add("Cliente 3");
+            cmbCliente.Items.Add("Cliente 4");
+
+            this.Controls.Add(lblCliente);
+            this.Controls.Add(cmbCliente);
+
+            // ==============================
+            // BOTÓN GUARDAR
+            // ==============================
+
+            btnGuardar = new Button();
+            btnGuardar.Text = "GUARDAR VENTA";
+            btnGuardar.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            btnGuardar.Location = new Point(170, 405);
+            btnGuardar.Size = new Size(190, 50);
+            btnGuardar.BackColor = Color.FromArgb(40, 167, 69);
+            btnGuardar.ForeColor = Color.White;
+            btnGuardar.FlatStyle = FlatStyle.Flat;
+            btnGuardar.FlatAppearance.BorderSize = 0;
+            btnGuardar.Cursor = Cursors.Hand;
+
+            btnGuardar.Click += BtnGuardar_Click;
+
+            this.Controls.Add(btnGuardar);
+
+            // ==============================
+            // BOTÓN LIMPIAR
+            // ==============================
+
+            btnLimpiar = new Button();
+            btnLimpiar.Text = "LIMPIAR";
+            btnLimpiar.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            btnLimpiar.Location = new Point(380, 405);
+            btnLimpiar.Size = new Size(190, 50);
+            btnLimpiar.BackColor = Color.FromArgb(108, 117, 125);
+            btnLimpiar.ForeColor = Color.White;
+            btnLimpiar.FlatStyle = FlatStyle.Flat;
+            btnLimpiar.FlatAppearance.BorderSize = 0;
+            btnLimpiar.Cursor = Cursors.Hand;
+
+            btnLimpiar.Click += BtnLimpiar_Click;
+
+            this.Controls.Add(btnLimpiar);
+        }
+
+        // ==============================
+        // CREAR LABEL
+        // ==============================
+
+        private Label CrearLabel(string texto, int x, int y)
+        {
+            Label label = new Label();
+
+            label.Text = texto;
+            label.Location = new Point(x, y);
+            label.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            label.ForeColor = Color.FromArgb(50, 50, 50);
+            label.AutoSize = true;
+
+            return label;
+        }
+
+        // ==============================
+        // BOTÓN GUARDAR
+        // ==============================
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            // Validar código
+            if (string.IsNullOrWhiteSpace(txtCodigo.Text))
+            {
+                MessageBox.Show(
+                    "Ingrese el código de la venta.",
+                    "Campo requerido",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                txtCodigo.Focus();
+                return;
+            }
+
+            // Validar producto
+            if (cmbProducto.SelectedIndex == -1)
+            {
+                MessageBox.Show(
+                    "Seleccione un tipo de producto.",
+                    "Campo requerido",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                cmbProducto.Focus();
+                return;
+            }
+
+            // Validar cliente
+            if (cmbCliente.SelectedIndex == -1)
+            {
+                MessageBox.Show(
+                    "Seleccione un cliente.",
+                    "Campo requerido",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                cmbCliente.Focus();
+                return;
+            }
+
+            // ==================================
+            // CREAR OBJETO DE LA CLASE DE ENTIDAD VENTAS (namespace EN)
+            // ==================================
+
+            ESFE._Clothing_Store.EN.Ventas venta = new ESFE._Clothing_Store.EN.Ventas();
+
+            venta.Codigo_de_Venta = txtCodigo.Text;
+            venta.Fecha_y_Hora = dtpFecha.Value;
+            venta.Cantidad_de_productos = (int)nudCantidad.Value;
+
+            // Por ahora usamos el índice como ID
+            venta.id_Tipo_Producto = cmbProducto.SelectedIndex + 1;
+            venta.id_cliente = cmbCliente.SelectedIndex + 1;
+
+            MessageBox.Show(
+                "Venta registrada correctamente.\n\n" +
+                "Código: " + venta.Codigo_de_Venta + "\n" +
+                "Fecha: " + venta.Fecha_y_Hora.ToString("dd/MM/yyyy HH:mm") + "\n" +
+                "Cantidad: " + venta.Cantidad_de_productos + "\n" +
+                "ID Producto: " + venta.id_Tipo_Producto + "\n" +
+                "ID Cliente: " + venta.id_cliente,
+                "Venta registrada",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        // ==============================
+        // BOTÓN LIMPIAR
+        // ==============================
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtCodigo.Clear();
+
+            dtpFecha.Value = DateTime.Now;
+
+            cmbProducto.SelectedIndex = -1;
+
+            nudCantidad.Value = 1;
+
+            cmbCliente.SelectedIndex = -1;
+
+            txtCodigo.Focus();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
