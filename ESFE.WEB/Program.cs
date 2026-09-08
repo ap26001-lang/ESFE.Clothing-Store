a752e829780cffad6209a196951ae90c5d5fce08
omare <omare@LENOVO>
2026-09-08 09:17:26 -0600

    Creacion del login

--- ESFE.WEB/Program.cs
+++ ESFE.WEB/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. Agregar servicios para Controladores con Vistas (MVC)
 builder.Services.AddControllersWithViews();
+
+// 2. Configurar la Autenticación basada en Cookies
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Home/Index";
+        options.AccessDeniedPath = "/Home/Index";
+        options.ExpireTimeSpan = TimeSpan.FromHours(8); // Duración de la sesión
+    });
 
 var app = builder.Build();
 
@@ -9,21 +20,22 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
+// 3. Middlewares de Seguridad (IMPORTANTE: El orden debe ser Authentication y luego Authorization)
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
+// 4. Configuración de la Ruta por Defecto (Redirige a Home/Index)
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
