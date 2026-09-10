bcb5eeff3f29d47601a033a8f5b55cdcf5ed00f9
omare <omare@LENOVO>
2026-09-10 10:09:05 -0600

    Creacion de web

--- ESFE.WEB/Controllers/HomeController.cs
+++ ESFE.WEB/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
 using ESFE.WEB.Models;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Security.Claims;
 
 namespace ESFE.WEB.Controllers
 {
@@ -12,59 +9,6 @@ namespace ESFE.WEB.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Login(string Email, string Password)
-        {
-            // Definición de roles y usuarios de prueba con nuevos dominios (Clave: 123456)
-            string nombre = "";
-            string rol = "";
-
-            if (Email == "admin@tienda.com" && Password == "123456")
-            {
-                nombre = "Administrador del Sistema";
-                rol = "Administrador";
-            }
-            else if (Email == "vendedor@tienda.com" && Password == "123456")
-            {
-                nombre = "Carlos Rivas (Vendedor)";
-                rol = "Vendedor";
-            }
-            else if (Email == "cliente@gmail.com" && Password == "123456")
-            {
-                nombre = "Sofía Mendoza (Cliente)";
-                rol = "Cliente";
-            }
-
-            // Si las credenciales son válidas
-            if (!string.IsNullOrEmpty(rol))
-            {
-                var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name, nombre),
-                    new Claim(ClaimTypes.Email, Email),
-                    new Claim(ClaimTypes.Role, rol)
-                };
-
-                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-                await HttpContext.SignInAsync(
-                    CookieAuthenticationDefaults.AuthenticationScheme,
-                    new ClaimsPrincipal(claimsIdentity));
-
-                return RedirectToAction("Index");
-            }
-
-            // Si las credenciales fallan
-            ViewBag.Error = "Correo o contraseña incorrectos";
-            return View("Index");
-        }
-
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
