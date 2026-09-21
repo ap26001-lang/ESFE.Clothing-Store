83ddbae1f2d29da112fd9e444d32ac7df4d7a73c
omare <omare@LENOVO>
2026-09-21 08:15:18 -0600

    Ajuste de DB.Comun

--- ESFE. Clothing Store.DAL/DBComun.cs
+++ ESFE. Clothing Store.DAL/DBComun.cs
@@ -10,22 +10,21 @@ namespace ESFE._Clothing_Store.DAL
     {
         // Conexion actualizada para usar la base TIENDA_ROPA
         // Nota: TrustServerCertificate=True permite confiar en el certificado del servidor (útil en desarrollo).
-        public const string _stringCnn = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TIENDA_ROPA;Integrated Security=True;TrustServerCertificate=True";
+        public const string _stringCnn = @"Data Source=localhost;Initial Catalog=TIENDA_ROPA;Integrated Security=True;TrustServerCertificate=True";
 
         /// <summary>
         /// Metodo para obtener base de datos.
         /// </summary>
         /// <returns>Devuelve la  conexion</returns>
         public static IDbConnection ObtenerConexion()
-            {
-                return new SqlConnection(_stringCnn);
-            }
+        {
+            return new SqlConnection(_stringCnn);
+        }
 
-            public static IDataReader ObtenerCommando(IDbConnection pConexion, string pSql)
-            {
-                SqlCommand _command = new SqlCommand(pSql, pConexion as SqlConnection);
-                return _command.ExecuteReader(CommandBehavior.CloseConnection);
-            }
+        public static IDataReader ObtenerCommando(IDbConnection pConexion, string pSql)
+        {
+            SqlCommand _command = new SqlCommand(pSql, pConexion as SqlConnection);
+            return _command.ExecuteReader(CommandBehavior.CloseConnection);
         }
     }
-
+}
